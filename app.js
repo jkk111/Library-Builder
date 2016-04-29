a5312a80e54d196fa379afc45d3415c7b746987c
John Kelly <kellyj58@tcd.ie>
2016-04-30 00:12:55 +0100

    Added the ability to run as a standalone app or module, can take rootName from command line

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 var app = require("express")();
-app.use(require("./index.js")({maxAge: 60 * 1000}));
+app.use();
+var buildLibrary = require("./index.js")();
 app.listen(80);
+app.get("/lib/:root", function(req, res, next) {
+  var libRoot = req.params.root;
+  buildLibrary(libRoot, function(err, result) {
+    if(err) return res.status(500).send("Error generating library!");
+    res.send(result);
+  })
+});
--- index.js
+++ index.js
@@ -1,115 +1,96 @@
 var fs = require("fs");
-var app = require("express")();
-var libraryDir, build, options;
+var libraryDir, options;
 
-module.exports = function(_options) {
-	options = _options || {};
-  options.path = (options.path  || "/library_builder") + "/:root";
-  options.dir = options.dir || "library/";
-  options.maxAge = options.maxAge || 0;
-  if(options.dir.charAt(options.dir.length - 1) != "/")
-    options.dir += "/"
-  app.get(options.path, function(req, res, next) {
-    var libRoot = req.params.root;
-    buildLibrary(libRoot, function(err, result) {
-      if(err) return res.status(500).send("Error generating library!");
-      res.send(result);
-    })
-  });
-  // buildLibrary("john", function(e) {
-  //   console.log(e == undefined ? "success" : "failure");
-  //   process.exit();
-  // })
-  return app;
+
+if (!module.parent) {
+  standalone();
+} else {
+  module.exports = init;
 }
 
-function formatFunction(array, divider, depth) {
-  var tmp = depth;
-  console.log(tmp, "localherre");
+function buildLibrary(libRoot, cb) {
+  var _root = "{";
+  var hasEntry = false;
+  if(libRoot.charAt(libRoot.length -1) != "/")
+    libRoot += "/";
+  var rootName = libRoot.substring(0, libRoot.length - 1);
+  var selfEval;
+  fs.writeFileSync("pre.js", handleDirectory(options.dir + libRoot, true))
+  eval(`selfEval = ${handleDirectory(options.dir + libRoot, true)}`);
+  var stringified = stringify_object(selfEval, options.minified == true ? undefined : options.delimiter || "  ");
+  if(cb) {
+    cb(null, stringified);
+  }
+  else {
+    fs.writeFileSync(`${__dirname}/${libRoot.substring(0, libRoot.length - 1)}.js`, `var ${libRoot.substring(0, libRoot.length - 1)} = ${stringified}`);
+    console.log("successfully wrote to:", __dirname + "/" + libRoot.substring(0, libRoot.length - 1) + ".js");
+  }
+}
+
+function formatFunction(array, divider, depth, lb) {
   var str = "";
+  // Indentation of the closing brace of the function
   var baseLine;
   if(array.length > 1) {
     baseLine = getLeadingWhitespace(array[array.length -1]);
   }
   for(var i = 0 ; i < array.length; i++) {
     var selfDepth = getLeadingWhitespace(array[i], depth);
-    if(i == 0)
-      str += array[i];
+    if(i == 0) {
+      if(divider)
+        str += array[i];
+      else
+        str += array[i].trim();
+    }
     else {
-      str += "\n" + getDivider(divider, depth) + getDivider(" ", selfDepth - baseLine) + (array[i].trim());
+      str += getDivider(divider, depth, lb) +
+             // Second get divider compensates for functions indentation
+             getDivider(divider == undefined ? "" :
+                        divider, Math.ceil((selfDepth - baseLine) / whiteSpaceValue(divider)), false) +
+                        (array[i].trim());
     }
   }
   return str;
 }
 
-function getDivider(divider, depth) {
+function getBaseName(name) {
+  return name.substring(name.lastIndexOf("/") , name.length - 3);
+}
+
+function getDirName(name) {
+  while(name.charAt(name.length -1) == "/") {
+    name = name.substring(0, name.length -1);
+  }
+  return name.substring(name.lastIndexOf("/") + 1);
+}
+
+function getDivider(divider, depth, lb) {
+  if(!divider)
+    return "";
   var str = "";
+  if(lb)
+    str += "\n";
   for (var i = 0; i < depth; i++) {
     str += divider;
   }
   return str;
 }
 
-function handleStringifyValue(key, value, divider, depth) {
-  if(typeof value === "object")
-   return stringify_object(value, divider, depth + 1);
-  else if(typeof value === "function")
-    return formatFunction(value.toString().split("\n"), divider, depth);
-  else
-    return value;
-}
-
-function stringify_object(o, divider, depth) {
-  depth = depth || 1;
-  var stringified = Array.isArray(o) ? "[" : "{";
-  var first = true;
-  console.log("DEPTH" + depth);
-  for(key in o) {
-    if(!first)
-      stringified += ",";
-    if(Array.isArray(o))
-      stringified += "\n" + getDivider(divider, depth) + handleStringifyValue(key, o[key], divider, depth);
+function getLeadingWhitespace(line, d) {
+  var patt = /(\t| )/;
+  var count = 0;
+  while(line.charAt(0).match(patt)) {
+    if(line.charAt(0) === "\t")
+      count += optiosn.tabSize || 4;
     else
-      stringified += "\n" + getDivider(divider, depth) + `'${key}': ${handleStringifyValue(key, o[key], divider, depth)}`;
-    first = false;
+      count++;
+    line = line.substring(1);
   }
-  if(divider)
-    stringified += "\n";
-  if(depth > 0)
-    stringified += getDivider(divider, depth - 1);
-  stringified += Array.isArray(o) ? "]" : "}";
-  return stringified;
+  return count;
 }
 
-function buildLibrary(libRoot, cb) {
-  if(build && build.time + options.maxAge > (new Date().getTime())) {
-    console.log("cached");
-    cb(null, build.data);
-  } else {
-    var _root = "{";
-    var hasEntry = false;
-      if(libRoot.charAt(libRoot.length -1) != "/")
-        libRoot += "/";
-      var rootName = libRoot.substring(0, libRoot.length - 1);
-      var selfEval;
-      console.log("here");
-      var dat = handleDirectory(options.dir + libRoot, true);
-      console.log(dat);
-      fs.writeFileSync("pre.js", handleDirectory(options.dir + libRoot, true));
-      eval(`selfEval = ${handleDirectory(options.dir + libRoot, true)}`);
-      console.log("and here")
-      fs.writeFileSync("selfEval.js", stringify_object(selfEval, "  "));
-      return cb(null, `var ${rootName} = ${stringify_object(selfEval, "  ")}`)
-      var _root = `var ${rootName} = ${selfEval}`;
-      build = { time: new Date().getTime(), data: `eval(\`${_root}\`);
-                if(${rootName}.init && typeof ${rootName}.init === "function") {
-                  ${rootName}.init();
-                }
-                else if(${rootName}.init && ${rootName}.init.ready && typeof ${rootName}.init.ready == "function") {
-                  ${rootName}.init.ready();
-                }`}
-      cb(null, build.data);
-  }
+function getNameEnding(name) {
+  return name.substring(name.length - 3)
 }
 
 function handleDirectory(path_to_dir, isRoot) {
@@ -122,7 +103,6 @@ function handleDirectory(path_to_dir, isRoot) {
     for(var i = 0 ; i < files.length; i++) {
       if(hasEntry) _root += ", ";
       var filename = files[i];
-      console.log(filename);
       var path_to_file = path_to_dir + filename;
       var stats = fs.statSync(path_to_file);
       if(stats.isDirectory()) {
@@ -133,37 +113,17 @@ function handleDirectory(path_to_dir, isRoot) {
       } else {
         var file = handleFile(path_to_file, true)
         if(file && !file.error) {
-          // _root[filename] = file;
           _root += `'${getBaseName(filename)}': ${file}`
         }
       }
       hasEntry = true;
     }
-    // if(isRoot)
-    //   console.log("here dir", dir);
     _root += "}";
-    if(!isRoot) {
-      // _root = `'${getDirName(path_to_dir)}': ${_root}`;
-    }
     return _root;
   } catch(e) {
     console.log(e, "handle dir");
     return {error: e};
   }
-}
-
-function getLeadingWhitespace(line, d) {
-  var patt = /(\t| )/;
-  var count = 0;
-  while(line.charAt(0).match(patt)) {
-    if(line.charAt(0) === "\t")
-      count += 4;
-    else
-      count++;
-    line = line.substring(1);
-  }
-  console.log(count, "CONT", d, line);
-  return count;
 }
 
 function handleFile(path_to_file, selfEval) {
@@ -174,24 +134,67 @@ function handleFile(path_to_file, selfEval) {
       filestr = filestr.substring(filestr.indexOf("{"));
     if(!selfEval)
       filestr = filestr.replace(/\\/g, "\\\\");
-    return filestr.replace(/\"/g, "'");
+    return filestr.replace(/'/g, "\\\'").replace(/\"/g, "'");
   } catch(e) {
     console.log(e, "handleFile");
     return {error: e}
   }
 }
 
-function getNameEnding(name) {
-  return name.substring(name.length - 3)
+function handleStringifyValue(key, value, divider, depth, lb) {
+  if(typeof value === "object")
+   return stringify_object(value, divider, depth + 1, lb);
+  else if(typeof value === "function")
+    return formatFunction(value.toString().split("\n"), divider, depth, lb);
+  else
+    return value;
 }
 
-function getBaseName(name) {
-  return name.substring(name.lastIndexOf("/") , name.length - 3);
+function init(_options) {
+  options = _options || {};
+  options.dir = options.dir || "library/";
+  options.minified = options.minified || false;
+  if(options.dir.charAt(options.dir.length - 1) != "/")
+    options.dir += "/"
+  return buildLibrary;
 }
 
-function getDirName(name) {
-  while(name.charAt(name.length -1) == "/") {
-    name = name.substring(0, name.length -1);
+function standalone() {
+  var options;
+  try { options = JSON.parse(fs.readFileSync("options.json")); } catch(e) {console.log(e);}
+  init(options)(process.argv[2]);
+}
+
+function stringify_object(o, divider, depth, lb) {
+  depth = depth || 1;
+  if(lb === undefined)
+    lb = divider !== undefined;
+  var stringified = Array.isArray(o) ? "[" : "{";
+  var first = true;
+  for(key in o) {
+    if(!first)
+      stringified += ",";
+    if(Array.isArray(o))
+      stringified += getDivider(divider, depth, lb) + handleStringifyValue(key, o[key], divider, depth, lb);
+    else
+      stringified += getDivider(divider, depth, lb) + `'${key}': ${handleStringifyValue(key, o[key], divider, depth, lb)}`;
+    first = false;
   }
-  return name.substring(name.lastIndexOf("/") + 1);
+  if(depth > 0)
+    stringified += getDivider(divider, depth - 1, lb);
+  stringified += Array.isArray(o) ? "]" : "}";
+  return stringified;
+}
+
+function whiteSpaceValue(delimiter) {
+  var val = 0;
+  for(var i = 0 ; i < delimiter.length; i++) {
+    var char = delimiter.charAt(i);
+    if(char === "\t") {
+      val += 4;
+    } else {
+      val += 1;
+    }
+  }
+  return val;
 }
